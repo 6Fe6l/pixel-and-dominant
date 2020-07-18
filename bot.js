06c54e34410de87c2eac6590f98245589a3cf6f2
6Fe6l <68379150+6Fe6l@users.noreply.github.com>
2020-07-18 08:43:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -648,3 +648,85 @@ let embed = new Discord.RichEmbed()
   {};
 } //
 });//
+
+
+client.on('message', message => {
+if(!message.channel.guild) return;
+if(message.content.startsWith(prefix + 'سحب')) {
+ if (message.member.hasPermission("MOVE_MEMBERS")) {
+ if (message.mentions.users.size === 0) {
+ return message.channel.send("``لاستخدام الأمر اكتب هذه الأمر : " +prefix+ "اسحب [USER]``")
+}
+if (message.member.voiceChannel != null) {
+ if (message.mentions.members.first().voiceChannel != null) {
+ var authorchannel = message.member.voiceChannelID;
+ var usermentioned = message.mentions.members.first().id;
+var embed = new Discord.RichEmbed()
+ .setTitle("Succes!")
+ .setColor("#000000")
+ .setDescription(`لقد قمت بسحب <@${usermentioned}> الى الروم الصوتي الخاص بك? `)
+var embed = new Discord.RichEmbed()
+.setTitle(`You are Moved in ${message.guild.name}`)
+ .setColor("RANDOM")
+.setDescription(`**<@${message.author.id}> Moved You To His Channel!\nServer --> ${message.guild.name}**`)
+ message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
+message.guild.members.get(usermentioned).send(embed)
+} else {
+message.channel.send("``لا تستطيع سحب "+ message.mentions.members.first() +" `يجب ان يكون هذه العضو في روم صوتي`")
+}
+} else {
+ message.channel.send("**``يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك``**")
+}
+} else {
+message.react("?")
+ }}});
+
+
+
+
+client.on('message', message => {
+  if (message == prefix +  "rooms") {
+    if(!message.channel.guild) return message.reply('**This command only for servers.**');
+ if (!message.member.hasPermission("ADMINISTRATOR")) return
+           let i = 1;
+         const men = message.guild.members.filter(m => m.hasPermission("DEAFEN_MEMBERS") && m.voiceChannel == null && m.presence.status !== 'offline').map(m=>`<@${m.id}>`);
+         message.channel.send(men.join(" "),{split:true})
+       }
+       })
+
+
+
+client.on('message',function(message) {
+  if(!message.channel.guild) return;
+
+const prefix = ".";
+    if (message.content === prefix + "discrim") {
+let messageArray = message.content.split(" ");
+let args = messageArray.slice(1);
+
+if (message.author.bot) return;
+
+var discri = args[0]
+let discrim
+if(discri){
+discrim = discri;
+}else{
+discrim = message.author.discriminator;
+}
+if(discrim.length == 1){
+discrim = "000"+discrim
+}
+if(discrim.length == 2){
+discrim = "00"+discrim
+}
+if(discrim.length == 3){
+discrim = "0"+discrim
+}
+
+const users = client.users.filter(user => user.discriminator === discrim).map(user => user.username);
+return message.channel.send(`
+**Found ${users.length} users with the discriminator #${discrim}**
+${users.join('\n')}
+`);
+}
+});
