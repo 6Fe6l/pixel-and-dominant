6adf84de5f3d7e1a31f3c48294a34d304004e060
6Fe6l <68379150+6Fe6l@users.noreply.github.com>
2020-07-21 00:08:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,57 +1,19 @@
 const Discord = require("discord.js");
-const myid = ['287898437058560000']
-const prefix = ['.'];
+const myid = ['569861608344518660'];
+const prefix = ['$'];
+const cmd = require("node-cmd")
 const client = new Discord.Client();
-const client2 = new Discord.Client();
-const client3 = new Discord.Client();
-const client4 = new Discord.Client();
-const client5 = new Discord.Client();
-const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
-const client10 = new Discord.Client();
-const client11 = new Discord.Client();
-const client12 = new Discord.Client();
-const client13 = new Discord.Client();
-const client14 = new Discord.Client();
-const client15 = new Discord.Client();
-const client16 = new Discord.Client();
-const client17 = new Discord.Client();
-const client18 = new Discord.Client();
-const client19 = new Discord.Client();
-const client20 = new Discord.Client();
-const client21 = new Discord.Client();
-const client22 = new Discord.Client();
-const client23 = new Discord.Client();
-const client24 = new Discord.Client();
-
+const Enmap = require("enmap")
+const Canvas = require('canvas')
+const moment = require("moment");
+require('moment-duration-format');
+const Ms = require("ms")
+const request = require("request");
+const hastebin = require("hastebin-paste");
+const fs = require("fs")
 
 
 client.login(process.env.TOKEN);
-client2.login(process.env.TOKEN2);
-client3.login(process.env.TOKEN3);
-client4.login(process.env.TOKEN4);
-client5.login(process.env.TOKEN5);
-client6.login(process.env.TOKEN6);
-client7.login(process.env.TOKEN7); 
-client8.login(process.env.TOKEN8); 
-client9.login(process.env.TOKEN9); 
-client10.login(process.env.TOKEN10); 
-client11.login(process.env.TOKEN11); 
-client12.login(process.env.TOKEN12); 
-client13.login(process.env.TOKEN13); 
-client14.login(process.env.TOKEN14); 
-client15.login(process.env.TOKEN15); 
-client16.login(process.env.TOKEN16); 
-client17.login(process.env.TOKEN17); 
-client18.login(process.env.TOKEN18); 
-client19.login(process.env.TOKEN19); 
-client20.login(process.env.TOKEN20); 
-client21.login(process.env.TOKEN21); 
-client22.login(process.env.TOKEN22); 
-client23.login(process.env.TOKEN23); 
-client24.login(process.env.TOKEN24); 
 
 
 
